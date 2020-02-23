fc56d65bed181f5aebb85809f26739dd3e9612c2
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-02-22 23:54:30 -0300

    Criacao do arquivo principal da aplicacao com o metodo main

---
+++ TesteFatorial.java
@@ -0,0 +1,7 @@
+class TesteFatorial{
+        public static void main(String[] args) {
+                Fatorial fat = new Fatorial();
+               System.out.println(fat.calcularFatorial(5));
+        }
+}
+        
